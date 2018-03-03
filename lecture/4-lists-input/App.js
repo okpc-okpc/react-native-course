99676a48f618e41de9eecbff0bc366413b8485a0
Dmitry <okpc.dn@gmail.com>
2018-03-03 16:20:29 +0200

    add sort functionality, place renderItem() to class

--- lecture/4-lists-input/App.js
+++ lecture/4-lists-input/App.js
@@ -2,26 +2,37 @@ import React from 'react';
 import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 import {Constants} from 'expo'
 
-import contacts from './contacts'
+import contacts, { compareNames } from './contacts'
 import Row from './Row'
 
 export default class App extends React.Component {
   state = {
     showContacts: false,
+    contacts: contacts,
   }
 
   toggleContacts = () => {
     this.setState(prevState => ({showContacts: !prevState.showContacts}))
   }
 
+  sort = () => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts].sort(compareNames),
+    }))
+  }
+
+  renderItem = obj => <Row {...(obj.item)} />
+  
+
   render() {
     return (
       <View style={styles.container}>
         <Button title="toggle contacts" onPress={this.toggleContacts} />
+        <Button title="sort" onPress={this.sort} />
         { this.state.showContacts && (
             <FlatList
-              renderItem={obj => <Row {...(obj.item)} />}
-              data={contacts}
+              renderItem={this.renderItem}
+              data={this.state.contacts}
             />
           )
         }
