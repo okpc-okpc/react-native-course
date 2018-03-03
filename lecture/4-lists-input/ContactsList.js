9b60b130176d139e6acea84016d3cbd5b201d35d
Dmitry <okpc.dn@gmail.com>
2018-03-03 19:53:22 +0200

    sepatate list by letter

--- lecture/4-lists-input/ContactsList.js
+++ lecture/4-lists-input/ContactsList.js
@@ -5,17 +5,28 @@ import Row from './Row'
 
 const renderItem = ({ item }) => <Row {...item} />
 
-const renderSectionHeader = obj => <Text>{obj.section.title}</Text>
+const renderSectionHeader = ({section}) => <Text>{section.title}</Text>
+
 
 const ContactsList = props => {
+	const contactsByLetter = props.contacts.reduce((obj, contact) => {
+		const firstLetter = contact.name[0].toUpperCase()
+		return {
+			...obj,
+			[firstLetter]: [...(obj[firstLetter] || []), contact],
+		}
+	}, {})
+	
+	const sections = Object.keys(contactsByLetter).sort().map(letter => ({
+		title: letter,
+		data: contactsByLetter[letter]
+	}))
+
 	return (
 		<SectionList
 			renderItem={renderItem}
 			renderSectionHeader={renderSectionHeader}
-			sections={[{
-				title: 'A',
-				data: props.contacts,
-			}]}
+			sections={sections}
 		/>
 	)
 }
